03ebe5d74a60ed76cbbbe36ba005ac347ffbb9e9
Geoffrey Jecinta <jecintageoffrey@yahoo.com>
2025-07-25 13:54:14 +0100

    Add EventsController with CRUD and Filter Endpoints

--- event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Event-Scheduler.Application/Interfaces/IEventService.cs
+++ event-scheduler-and-conflict-detector/event-scheduler-and-conflict-detector-api/Event-Scheduler.Application/Interfaces/IEventService.cs
@@ -1,6 +1,16 @@
-﻿namespace event_scheduler_and_conflict_detector_api.Event.Application.Interfaces
+﻿using event_scheduler_and_conflict_detector_api.Event.Application.Dtos;
+using event_scheduler_and_conflict_detector_api.Event.Domain.Models;
+
+namespace event_scheduler_and_conflict_detector_api.Event.Application.Interfaces
 {
     public interface IEventService
     {
+        IEnumerable<Domain.Models.Event> GetAllEvent();
+        Domain.Models.Event GetEventById(Guid id);
+        IEnumerable<Domain.Models.Event> GetEventsByDate(DateTime date);
+        IEnumerable<Domain.Models.Event> GetEventsInRanges(DateTime start, DateTime end);
+        void AddEvent(EventCreateDto dto);
+        void UpdateEvent(Guid id, EventUpdateDto dto);
+        bool DeleteEvent(Guid id);
     }
 }
